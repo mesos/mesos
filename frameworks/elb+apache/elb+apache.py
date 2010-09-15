cde35bb891975a250ed99511e2d7903addfdc3b6
Andy Konwinski <andyk@berkeley.edu>
2010-09-15 00:35:30 -0700

    Added lock to monitor(), removes all intances from elastic load balancer in the scheduler constructor, refactored to conform to 80 column max coding style, fixed bug in parsing cloudwatch RequstCount query results (now using max to get most recent RequestCount statistic).

--- frameworks/elb+apache/elb+apache.py
+++ frameworks/elb+apache/elb+apache.py
@@ -16,7 +16,7 @@ from subprocess import *
 
 MIN_SERVERS = 1
 LOAD_BALANCER_NAME = "my-load-balancer"
-TARGET_CONN_PER_MIN_PER_BACKEND = 25 * 60 #This is probably still a bit too low
+TARGET_CONN_PER_MIN_PER_BACKEND = 5 * 60 #This is probably still a bit too low
 
 class ApacheWebFWScheduler(mesos.Scheduler):
   def __init__(self):
@@ -34,7 +34,12 @@ class ApacheWebFWScheduler(mesos.Scheduler):
     self.metrics = self.cw_conn.list_metrics()
     print self.metrics[13]
     self.host_map = updated_host_map()
+    self.kill_list = [] # a list of task ids we called killTask on but haven't 
+                        # received a status update with status = KILLEd yet for
     self.elb_conn = boto.connect_elb()
+    #reset the load balancer to have 0 back ends registered
+    self.elb_conn.deregister_instances(LOAD_BALANCER_NAME, 
+                                       self.host_map.values())
 
   def registered(self, driver, fid):
     print "Mesos elb+apache scheduler registered as framework #%s" % fid
@@ -48,103 +53,102 @@ class ApacheWebFWScheduler(mesos.Scheduler):
     return mesos.ExecutorInfo(execPath, "")
 
   def resourceOffer(self, driver, oid, slave_offers):
-    print "Got resource offer %s with %s slots." % (oid, len(slave_offers))
+    print "\nGot resource offer %s with %s slots." % (oid, len(slave_offers))
     self.lock.acquire()
     tasks = []
+    nodes_used, no_more_needed = 0, 0
     for offer in slave_offers:
       if offer.host in self.servers.values():
-        print "Rejecting slot on host " + offer.host + " because we've launched a server on that machine already."
-        #print "self.servers currently looks like: " + str(self.servers)
+        nodes_used += 1
       elif len(self.servers) >= self.desired_servers and len(self.servers) > 0:
-        print "Rejecting slot because we've launched enough tasks."
+        no_more_needed += 1
       elif int(offer.params['mem']) < 1024:
-        print "Rejecting offer because it doesn't contain enough memory (it has " + offer.params['mem'] + " and we need 1024mb."
+        print "Rejecting offer because it doesn't contain enough memory" + \
+              "(it has " + offer.params['mem'] + " and we need 1024mb."
       elif int(offer.params['cpus']) < 1:
         print "Rejecting offer because it doesn't contain enough CPUs."
       else:
-        print "len(self.servers) = " + str(len(self.servers)) + ", self.desired_servers  = " + str(self.desired_servers) + ", len(self.servers) = " + str(len(self.servers))
-        print "Offer is for " + offer.params['cpus'] + " CPUS and " + offer.params["mem"] + " MB on host " + offer.host
+        print "len(self.servers) = " + str(len(self.servers)) + \
+              ", self.desired_servers  = " + str(self.desired_servers)
+        print "Offer is for " + offer.params['cpus'] + " CPUS and " + \
+              offer.params["mem"] + " MB on host " + offer.host
         params = {"cpus": "1", "mem": "1024"}
-        td = mesos.TaskDescription(self.id, offer.slaveId, "server %s" % self.id, params, "")
-        print "Accepting task, id=" + str(self.id) + ", params: " + params['cpus'] + " CPUS, and " + params['mem'] + " MB, on node " + offer.host
+        td = mesos.TaskDescription(self.id, offer.slaveId,
+                                   "server %s" % self.id, params, "")
+        print "Accepting task, id=" + str(self.id) + ", params: " + \
+              params['cpus'] + " CPUS, and " + params['mem'] + \
+              " MB, on node " + offer.host
         tasks.append(td)
         self.servers[self.id] = offer.host
         self.id += 1
         print "self.servers length is now " + str(len(self.servers))
     driver.replyToOffer(oid, tasks, {"timeout":"1"})
-    #driver.replyToOffer(oid, tasks, {})
-    print "done with resourceOffer()"
+    if nodes_used > 0:
+      print ("Rejecting %d slots because we've launched servers on those " + \
+             "machines already.") % nodes_used
+    if no_more_needed > 0:
+      print "Rejecting %d slot because we've launched enough tasks." % \
+            no_more_needed
+    print "Done with resourceOffer()"
     self.lock.release()
 
   def statusUpdate(self, driver, status):
-    print "received status update from taskID " + str(status.taskId) + ", with state: " + str(status.state)
-    reconfigured = False
+    print "\nReceived status update from taskID " + str(status.taskId) + \
+          ", with state: " + str(status.state)
     self.lock.acquire()
     if not status.taskId in self.servers.keys():
-      print "This status was from a node where server wasn't (supposed to be) running"
+      print "This status was from a node where the server wasn't " + \
+            "(supposed to be) running."
     else:
+      print "Parsing and handling status update."
       if status.state == mesos.TASK_STARTING:
-        print "Task " + str(status.taskId) + " reported that it is STARTING."
+        print "Task " + str(status.taskId) + " reported that it's STARTING."
         del self.servers[status.taskId]
       if status.state == mesos.TASK_RUNNING:
-        print "Task " + str(status.taskId) + " reported that it is RUNNING, reconfiguring elb to include it in webfarm now."
-        print "Adding task's host node to load balancer " + LOAD_BALANCER_NAME
+        print ("Task %s reported that it's RUNNING, reconfiguring elb to " + 
+              "include it in webfarm now.") % str(status.taskId)
+        print "Adding task's host node to load balancer %s." % \
+              LOAD_BALANCER_NAME
         host_name = self.servers[status.taskId]
-        print "Task's hostname is " + host_name
-        print "self.host_map is " + str(self.host_map)
+        print "Task's hostname is %s." % host_name
         instance_id = self.host_map[host_name]
         print "Task's instance id is " + instance_id
-        lbs = self.elb_conn.register_instances(LOAD_BALANCER_NAME, [instance_id])
-        print "Load balancer reported all backends as: " + str(lbs)
+        lbs = self.elb_conn.register_instances(LOAD_BALANCER_NAME, 
+                                               [instance_id])
+        print "Load balancer reported all backends as: %s." % str(lbs)
       if status.state == mesos.TASK_FINISHED:
         del self.servers[status.taskId]
-        print "Task " + str(status.taskId) + " reported FINISHED (state " + status.state + ")."
+        print "Task %s reported FINISHED (state %s)." % \
+              str(status.taskId), str(status.state)
       if status.state == mesos.TASK_FAILED:
-        print "Task " + str(status.taskId) + " reported that it FAILED!"
+        print "Task %s reported that it FAILED!" % str(status.taskId)
         del self.servers[status.taskId]
       if status.state == mesos.TASK_KILLED:
-        print "Task " + str(status.taskId) + " reported that it was KILLED!"
+        print "Task %s reported that it was KILLED!" % str(status.taskId)
         del self.servers[status.taskId]
       if status.state == mesos.TASK_LOST:
-        print "Task " + str(status.taskId) + " reported was LOST!"
+        print "Task %s was reported as LOST!" % str(status.taskId)
         del self.servers[status.taskId]
     self.lock.release()
-    print "done in statusupdate"
+    print "Done in statusupdate()."
 
   def kill_backends(self, num):
-    print "in kill_backends, killing %i backends" % num
+    print "In kill_backends(), killing %i backends" % num
     host_names, host_ids = [], []
     for m,n in self.servers.items()[:num]:
       host_ids.append(m)
       host_names.append(n)
-    print "host_names is " + str(host_names)
+    print "host_names[] is " + str(host_names) + "."
     instance_ids = [self.host_map[i] for i in host_names]
-    print "deregistering instance ids from elb: " + str(instance_ids)
+    print "Deregistering instance ids from elb: " + str(instance_ids) + "."
     lbs = self.elb_conn.deregister_instances(LOAD_BALANCER_NAME, instance_ids)
-    print "calling driver.kill_task on tasks: " + str(host_ids) 
+    print "Calling driver.kill_task on tasks: " + str(host_ids) + "."
     [self.driver.killTask(i) for i in host_ids]
+    [self.kill_list.append(i) for i in host_ids]
     #The following (i.e. removing host from self.servers) will happen when the
     #task's status is reported to the FW as KILLED
     #print "removing task from servers list: " + str(host_ids) 
     #[self.servers.pop(int(i)) for i in host_ids]
-
-#  def scaleUp(self):
-#    print "SCALING UP"
-#    self.lock.acquire()
-#    self.overloaded = True
-#    self.lock.release()
-#
-#  def scaleDown(self, id):
-#    print "SCALING DOWN (removing server %d)" % id
-#    kill = False
-#    self.lock.acquire()
-#    if self.overloaded:
-#      self.overloaded = False
-#    else:
-#      kill = True
-#    self.lock.release()
-#    if kill:
-#      self.driver.killTask(id)
 
 
 def updated_host_map():
@@ -157,52 +161,38 @@ def updated_host_map():
 
 def monitor(sched):
   while True:
-    print ">>>>>>> In monitor()"
     #ELB only reports "metrics" every minute at its most fine granularity
     time.sleep(2)
-    print "done sleeping"
+    print "\nIn monitor() loop. Done sleeping, acquiring lock."
+    sched.lock.acquire()
     try:
-      #looking for the RequestCount metric for our load balancer 
-      request_count = sched.metrics[13]
-      print request_count
-      result = request_count.query(datetime.datetime.now()-datetime.timedelta(minutes=2),datetime.datetime.now(), 'Sum', 'Count', 60)
-      print result
+      #get the RequestCount metric for our load balancer 
+      rct = [m for m in sched.metrics if str(m) == "Metric:RequestCount"][0]
+      result = rct.query(datetime.datetime.now()-datetime.timedelta(minutes=1),
+                                   datetime.datetime.now(), 'Sum', 'Count', 60)
+      print "Request count query returned: %s" % result
       if len(result) == 0:
         sched.desired_servers = 1
-        print "RequestCount was 0, so set sched.desired_servers to " + str(sched.desired_servers)
+        print "RequestCount was 0, so set sched.desired_servers to " + \
+              str(sched.desired_servers)
       else:
         #TODO(andyk): Probably want to weight this to smooth out ups and downs
-        new_num_servers = int(result[0]["Sum"]/TARGET_CONN_PER_MIN_PER_BACKEND)
+        r = max(result, key=lambda x: x["Timestamp"])
+        new_num_servers = int(r["Sum"] / TARGET_CONN_PER_MIN_PER_BACKEND)
         sched.desired_servers = max(new_num_servers,1)
-        print "RequestCount was " + str(result[0]["Sum"]) + ", setting sched.desired_servers = " + str(sched.desired_servers) 
+        print "RequestCount was " + str(result[0]["Sum"]) + \
+              ", setting sched.desired_servers = " + str(sched.desired_servers) 
       print "len(sched.servers) is " + str(len(sched.servers))
       if sched.desired_servers < len(sched.servers):
-        print "time to kill some servers"
+        print "Time to kill some servers"
         num_to_kill = len(sched.servers)-sched.desired_servers
-        print "removing %i backends" % num_to_kill
+        print "Removing %i backends" % num_to_kill
         sched.kill_backends(num_to_kill)
-      print "<<<<<<<< done with monitor()"
-
-#        if int(data[33]) >= START_THRESHOLD:
-#          sched.scaleUp()
-#        elif int(data[4]) <= KILL_THRESHOLD:
-#          minload, minid = (sys.maxint, 0)
-#          for l in lines:
-#            cols = l.split(',')
-#            id = int(cols[1])
-#            load = int(cols[4])
-#            if load < minload:
-#              minload = load
-#              minid = id
-#
-#          if len(lines) > MIN_SERVERS and minload == 0:
-#            sched.scaleDown(minid)
-#
-#        conn.close()
     except Exception, e:
-      print "exception in monitor()" + str(e)
+      print "Exception in monitor()" + str(e)
       continue
-  print "done in MONITOR()"
+    print "Done with monitor() loop, releasing lock."
+    sched.lock.release()
 
 if __name__ == "__main__":
   parser = OptionParser(usage = "Usage: %prog mesos_master")
@@ -222,6 +212,5 @@ if __name__ == "__main__":
   threading.Thread(target = monitor, args=[sched]).start()
 
   driver.run()
-
 
   print "Scheduler finished!"
