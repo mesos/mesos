44575712d4d7544dfe4b3242db8b3471ccbbb9f8
Matei Zaharia <matei@eecs.berkeley.edu>
2010-09-30 17:47:45 -0700

    Increased SSH connect timeout in EC2 scripts to 5 seconds because sometimes commands were timing out prematurely even for working slave nodes

--- ec2/deploy.lucid64/root/hadoop-0.20.2/conf/hadoop-env.sh
+++ ec2/deploy.lucid64/root/hadoop-0.20.2/conf/hadoop-env.sh
@@ -30,7 +30,7 @@ export HADOOP_JOBTRACKER_OPTS="-Dcom.sun.management.jmxremote $HADOOP_JOBTRACKER
 
 # Extra ssh options.  Empty by default.
 # export HADOOP_SSH_OPTS="-o ConnectTimeout=1 -o SendEnv=HADOOP_CONF_DIR"
-export HADOOP_SSH_OPTS="-o ConnectTimeout=2"
+export HADOOP_SSH_OPTS="-o ConnectTimeout=5"
 
 # Where log files are stored.  $HADOOP_HOME/logs by default.
 # export HADOOP_LOG_DIR=${HADOOP_HOME}/logs
